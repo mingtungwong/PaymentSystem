634320646da424277b19fc20997d0d61cf01610d
Ming <mingtungwong@outlook.com>
2018-08-22 15:53:00 -0400

    Beging implementing transaction/invoice

---
+++ Transaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentSystem
+{
+    public class Transaction
+    {
+        public static int TransactionIDCounter = 1;
+        public ObservableCollection<TransactionLine> Items { get; set; }
+        public ObservableCollection<PaymentLine> Payments { get; set; }
+        public string TransactionName { get; set; }
+        public int TransactionIDNumber { get; set; }
+
+        public Transaction(string name)
+        {
+            TransactionIDNumber = TransactionIDCounter++;
+            TransactionName = name;
+            Items = new ObservableCollection<TransactionLine>();
+            Payments = new ObservableCollection<PaymentLine>();
+        }
+
+        public void AddItem(TransactionLine item)
+        {
+            Items.Add(item);
+        }
+
+        public void AddPayment(PaymentLine payment)
+        {
+            Payments.Add(payment);
+        }
+        
+    }
+}
---
+++ TransactionLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentSystem
+{
+    public class TransactionLine
+    {
+        public string ItemName { get; set; }
+        public double ItemCost { get; set; }
+
+        public TransactionLine(string itemName, double itemCost)
+        {
+            ItemName = itemName;
+            ItemCost = itemCost;
+        }
+    }
+}
